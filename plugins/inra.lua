86e024ee68c9f613016011f2bc89f61d1e70c0c2
mohammad12344 <sdf12345641@gmail.com>
2016-03-21 02:02:49 +0430

    Update inra.lua

--- plugins/inra.lua
+++ plugins/inra.lua
@@ -1,24 +1,6 @@
 do
 function run(msg, matches)
 local reply_id = msg['id']
-local Arian = 144616352
-	if msg.from.id == tonumber(Arian) then
-		 text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
-		elseif is_sudo(msg) then
-		 text = text..'مقام : ادمین ربات (Admin) \n\n'
-		elseif is_owner(msg) then
-		 text = text..'مقام : مدیر کل گروه (Owner) \n\n'
-		elseif is_momod(msg) then
-		 text = text..'مقام : مدیر گروه (Moderator) \n\n'
-		else
-		 text = text..'مقام : کاربر (Member) \n\n'
-		end
-	  else
-	   text = text..'مقام : '..value..'\n'
-	  end
-	end
-    
-
 local info = '#Name : '..msg.from.first_name..'\n\n'
 ..'♦ایدی : '..msg.from.id..'\n'
 ..'♦نام کاربری : @'..msg.from.username..'\n\n'
